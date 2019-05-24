74ace73bf09e612b258db2f999bc7aca162e4a37
BrunoSette <brunosette@gmail.com>
2019-05-23 21:10:23 -0300

    Init commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Bruno!");
+
+        int myFirstVariable = 5;
+        System.out.println(myFirstVariable);
+        
+
+
+    }
+
+
+}
